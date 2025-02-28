6ac020fe6129e45a24a49e2da2786858d36efa54
digodogit <rodrigo.quaglio@hotmail.com>
2025-02-28 09:42:00 -0300

    atualização do RAG

--- backend/config/repositories/RAG_repository.py
+++ backend/config/repositories/RAG_repository.py
@@ -4,8 +4,8 @@ from config.models import RagUserFiles, RagVectorStore, Thread  # Importando a c
 
 class MongoRAGRepository:
     def __init__(self, client: AsyncIOMotorClient):
-        """Inicializa a conexão com a coleção 'threads' no banco 'playground_DB'."""
-        self.collection = client.threads
+        """Inicializa a conexão com a coleção 'RAG' no banco 'test_playground_DB'."""
+        self.collection = client.rags
 
     async def create_vector_store(self, new_vector: RagVectorStore):
         """Cria uma nova thread no banco de dados."""
@@ -15,7 +15,7 @@ class MongoRAGRepository:
                 "name": new_vector.name,
                 "file_ids": new_vector.get("file_ids",[])
             })
-
+    
             return await self.get_vector_store(new_vector.id) if document.inserted_id else None
         except PyMongoError as e:
             print(f"Erro ao registrar thread: {e}")
@@ -47,7 +47,7 @@ class MongoRAGRepository:
         try:
             document = await self.collection.insert_one({
             "file_id": new_file.id,
-            "user_email": RagUserFiles,
+            "user_email": new_file.user_email,
             "purpose": new_file.purpose,
             "file_attach": new_file.file_attach
         })
@@ -75,59 +75,13 @@ class MongoRAGRepository:
             print(f"Erro ao excluir file: {e}")
 
 
-            return None
-    async def update_assistant_thread(self, thread_id: str, assistant_id: str):
-        try:
-            result = await self.collection.update_one({"id": thread_id},
-                    
-                                                  {"$set": {"assistant_id": assistant_id}})
-            return result
-                
-        except PyMongoError as e:
-            print(f"Erro ao adicionar assistant thread: {e}")
-            return None
-        
-    async def update_thread_message(self, new_message_id: str, thread_id: str):
-        try:
-            result = await self.collection.update_one({"id": thread_id},
-                    
-                                                  {"$push": {"messages": new_message_id}})
-            return result
-                
-        except PyMongoError as e:
-            print(f"Erro ao adicionar thread message: {e}")
-            return None
-        
-    async def delete_thread_message(self, message_id: str, thread_id: str):
-
-        try:
-            result = await self.thread_collection.update_one(
-                    {"id": thread_id},
-                    {"$pull": {"messages": message_id}}
-                )
-            if result.deleted_count > 0:
-                return result
-            return False
-        except PyMongoError as e:
-            print(f"Erro ao excluir mensagem na thread: {e}")
-            return None
     
-    async def update_thread_runs(self, new_run_id: str, thread_id: str):
-        try:
-            result = await self.collection.update_one({"id": thread_id},
-                    
-                                                  {"$push": {"threads": new_run_id}})
-            return result
-                
-        except PyMongoError as e:
-            print(f"Erro ao adicionar thread run: {e}")
-            return None
-        
 
-    def __to_thread_model(self, obj: dict) -> Thread:
+    def __to_files_model(self, obj: dict) -> Thread:
         """Converte um documento do MongoDB para um objeto Thread."""
         return Thread(
-            id=obj["id"],
-            messages=obj.get("messages", []),
-            runs=obj.get("runs", [])
+            files_id=obj["files_id"],
+            purpose=obj.get("purpose"),
+            user_email=obj.get("user_email"),
+            file_attach=obj["file_attach"]
         )
